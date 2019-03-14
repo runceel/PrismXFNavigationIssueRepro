0b3c0a108c16b13d3129d07fb7f838f094f51a68
Kazuki Ota <kaota@microsoft.com>
2019-03-14 12:58:11 +0900

    Add project files.

---
+++ BlankApp1/BlankApp1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace BlankApp1.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private DelegateCommand _navigateCommand;
+        public DelegateCommand NavigateCommand =>
+            _navigateCommand ?? (_navigateCommand = new DelegateCommand(ExecuteNavigateCommand));
+
+        private async void ExecuteNavigateCommand()
+        {
+            await NavigationService.NavigateAsync("NextPage");
+        }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            Debug.WriteLine("MainPageViewModel#OnNavigatedTo is called");
+        }
+    }
+}
---
+++ BlankApp1/BlankApp1/Views/NextPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BlankApp1.Views
+{
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
